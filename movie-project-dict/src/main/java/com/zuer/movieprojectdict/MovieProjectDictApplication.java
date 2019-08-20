e27e038fce9cb196f091cde2d551a7c2d09b6827
zuer <1974900537@example.com>
2019-08-20 16:34:09 +0800

    配置eureka的客户端和服务端

--- movie-project-dict/src/main/java/com/zuer/movieprojectdict/MovieProjectDictApplication.java
+++ movie-project-dict/src/main/java/com/zuer/movieprojectdict/MovieProjectDictApplication.java
@@ -2,7 +2,13 @@ package com.zuer.movieprojectdict;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@EnableEurekaClient
+@RestController
 @SpringBootApplication
 public class MovieProjectDictApplication {
 
--- movie-project-eureka/src/main/java/com/zuer/movieprojecteureka/MovieProjectEurekaApplication.java
+++ movie-project-eureka/src/main/java/com/zuer/movieprojecteureka/MovieProjectEurekaApplication.java
@@ -2,10 +2,11 @@ package com.zuer.movieprojecteureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-//@EnableEurekaServer //启用Eureka Server
-@SpringBootApplication
+@EnableEurekaServer //启用Eureka Server
+@SpringBootApplication(exclude ={DataSourceAutoConfiguration.class} )
 public class MovieProjectEurekaApplication {
 
     public static void main(String[] args) {
