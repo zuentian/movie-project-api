305b8b4ed727d1796e4fb63b6beea6a09805df01
zuer <1974900537@qq.com>
2019-08-21 00:08:38 +0800

    整合feign

--- movie-project-common/src/main/java/com/zuer/movieprojectcommon/service/DictService.java
+++ movie-project-common/src/main/java/com/zuer/movieprojectcommon/service/DictService.java
@@ -4,6 +4,8 @@ import com.zuer.movieprojectcommon.entity.Dict;
 import com.zuer.movieprojectcommon.entity.DictValue;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -12,5 +14,6 @@ public interface DictService {
 
 
     List<DictValue> queryDictByDictType(@Param("dictType")String dictType);
+    @PostMapping("/queryDict")
     List<Dict> queryDict();
 }
--- movie-project-user/src/main/java/com/zuer/movieprojectuser/feignConfig/CommonFeignClient.java
+++ movie-project-user/src/main/java/com/zuer/movieprojectuser/feignConfig/CommonFeignClient.java
@@ -4,10 +4,7 @@ package com.zuer.movieprojectuser.feignConfig;
 import com.zuer.movieprojectcommon.entity.Dict;
 import com.zuer.movieprojectcommon.entity.DictValue;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
@@ -18,6 +15,6 @@ public interface CommonFeignClient {
     @RequestMapping(value = "/queryDictByDictType/{dictType}",method = RequestMethod.GET)
     public List<DictValue> queryDictByDictType(@PathVariable("dictType")String dictType);
 
-    @RequestMapping(value = "/queryDict",method = RequestMethod.POST)
+    @PostMapping(value = "/queryDict")
     public List<Dict> queryDict();
 }
