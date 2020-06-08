2376b9a4527feae7463a049a5481bdebc2d733c0
zuer <1974900537@example.com>
2020-06-08 15:42:10 +0800

    本地kafka启动bat文件

--- Document/start_kafka_windows.bat
+++ Document/start_kafka_windows.bat
@@ -1,9 +1,12 @@
+#启动zookeeper
 start  zkServer
 
+#启动kafka，端口520
 E:
 cd \kafka\kafka_2.12-2.5.0
 start .\bin\windows\kafka-server-start.bat .\config\server.properties
 
+#启动kafka-manager管理工具，端口521
 E:
 cd \kafka\kafka-manager-1.3.3.17
 del RUNNING_PID
