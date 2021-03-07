236a8246eb3953bab88d728f5c91e339d526d093
zuer <1974900537@qq.com>
2021-03-07 22:56:13 +0800

    多线程的基础知识

--- threadbasic/src/JAVA高并发编程详解/chapter17/ReadLock.java
+++ threadbasic/src/JAVA高并发编程详解/chapter17/ReadLock.java
@@ -10,11 +10,27 @@ class ReadLock implements Lock {
 
     @Override
     public void lock() throws InterruptedException {
-
+        synchronized (readWriteLock.getMUTEX()){
+            //若此时有线程正在进行写操作，或者有写线程在等待并且偏向写锁的标识为true时
+            //就会无法获得读锁，只能被挂起
+            while (readWriteLock.getWritingWriters() >0 ||
+                    (readWriteLock.getPreferWriter() && readWriteLock.getWaitingWriters() > 0)){
+                readWriteLock.getMUTEX().wait();
+            }
+            //成功获得读锁，并且使readingReaders的数量增加
+            readWriteLock.incrementReadingReaders();
+        }
     }
 
     @Override
     public void unlock() {
-
+        synchronized (readWriteLock.getMUTEX()){
+            //释放锁的过程就是使得当前reading的数量减一
+            //将perferWriter设置为true,可以使得writer线程获得更多的机会
+            //通知唤醒与Mutex关联的监听器 wait set中线程
+            readWriteLock.decrementReadingReaders();
+            readWriteLock.changePrefer(true);
+            readWriteLock.getMUTEX().notifyAll();
+        }
     }
 }
--- threadbasic/src/JAVA高并发编程详解/chapter17/WriteLock.java
+++ threadbasic/src/JAVA高并发编程详解/chapter17/WriteLock.java
@@ -8,6 +8,23 @@ class WriteLock implements Lock{
 
     @Override
     public void lock() throws InterruptedException {
+        synchronized (readWriteLock.getMUTEX()){
+            //首先使等待获取写入锁的数字加一
+            try {
+                readWriteLock.incrementWaitingWriters();
+                //如果此时有其他线程正在进行读操作，或者写操作，那么当前线程将被挂起
+                while (readWriteLock.getReadingReaders() > 0 ||
+                        readWriteLock.getWritingWriters() > 0){
+                    readWriteLock.getMUTEX().wait();
+                }
+            } finally {
+                //成功获取到了写入锁，使得等待获取写入锁的计数器减一
+                this.readWriteLock.decrementWaitingWriters();
+            }
+            //将正在写入的线程数量加一
+            readWriteLock.incrementWritingWriters();
+
+        }
 
     }
 
