5ed3eda768ff11b99f8ef0ef3233dd30af7ff2aa
zuer <1974900537@example.com>
2021-07-27 15:49:08 +0800

    新增git分支，项目切换成mysql

--- zuer-lvdouban-auth/src/main/java/com/zuer/zuerlvdoubanauth/ZuerLvdoubanAuthApplication.java
+++ zuer-lvdouban-auth/src/main/java/com/zuer/zuerlvdoubanauth/ZuerLvdoubanAuthApplication.java
@@ -1,13 +1,10 @@
 package com.zuer.zuerlvdoubanauth;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.bind.annotation.RestController;
 
 
 /*
@@ -17,9 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 数据字典的管理
  */
 
-@RestController
-@SpringBootApplication(exclude ={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-@MapperScan(basePackages = {"com.zuer.zuerlvdoubanauth"})
+//@RestController
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@MapperScan(basePackages = {"com.zuer.zuerlvdoubanauth"})
 @EnableEurekaClient
 @EnableFeignClients
 public class ZuerLvdoubanAuthApplication {
--- zuer-lvdouban-movie/src/main/java/com/zuer/zuerlvdoubanmovie/ZuerLvdoubanMovieApplication.java
+++ zuer-lvdouban-movie/src/main/java/com/zuer/zuerlvdoubanmovie/ZuerLvdoubanMovieApplication.java
@@ -1,19 +1,16 @@
 package com.zuer.zuerlvdoubanmovie;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+//@RestController
 @SpringBootApplication(exclude ={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
-@MapperScan(basePackages = {"com.zuer.zuerlvdoubanmovie"})
+//@MapperScan(basePackages = {"com.zuer.zuerlvdoubanmovie"})
 @EnableEurekaClient
 @EnableFeignClients
 public class ZuerLvdoubanMovieApplication {
