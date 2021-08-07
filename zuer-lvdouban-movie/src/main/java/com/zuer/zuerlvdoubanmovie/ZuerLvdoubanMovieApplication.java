d4a62e5b0247f476a01d3982c735dd5d83185475
zuer <1974900537@example.com>
2021-08-07 12:29:30 +0800

    修改配置

--- zuer-lvdouban-movie/src/main/java/com/zuer/zuerlvdoubanmovie/ZuerLvdoubanMovieApplication.java
+++ zuer-lvdouban-movie/src/main/java/com/zuer/zuerlvdoubanmovie/ZuerLvdoubanMovieApplication.java
@@ -3,13 +3,12 @@ package com.zuer.zuerlvdoubanmovie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 //@RestController
-@SpringBootApplication(exclude ={DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
+@SpringBootApplication(exclude ={DataSourceAutoConfiguration.class})
 //@MapperScan(basePackages = {"com.zuer.zuerlvdoubanmovie"})
 @EnableEurekaClient
 @EnableFeignClients
